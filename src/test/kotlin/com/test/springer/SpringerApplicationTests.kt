e1e9fb5e54518b846913a287d87d5f3e52613879
jmccormick@tarkenton.com <jmccormick@tarkenton.com>
2023-10-16 11:13:48 -0400

    initial

---
+++ src/test/kotlin/com/test/springer/SpringerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.springer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
